93ef4e3441f937af66cafe456a2e5ef69d519c23
lucifer_112 <Krishnafauj02@gmail.com>
2025-03-22 00:37:37 +0530

    new project added

---
+++ project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Blocks from './components/Home/Blocks'
+import '@fontsource/dm-sans';
+import '@fontsource/dm-sans/400.css';
+import '@fontsource/dm-sans/500.css';
+import '@fontsource/dm-sans/700.css';
+import Topblock from './components/Home/Topblock';
+import './App.css'
+function App() {
+  return (
+    <div className='  bg-[#f5f5f8] min-h-screen '>
+      <Topblock/>
+      <Blocks/>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ project/src/components/Home/Blocks.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const data = [
+    {
+        "title": "Automated Data Collection",
+        "description": "Our system automates carbon data collection, saving time, reducing errors, and ensuring accurate sustainability reporting."
+    },
+    {
+        "title": "Monitoring & Reporting",
+        "description": "Effortlessly track and report carbon emissions with automated monitoring, ensuring accuracy and compliance in sustainability reporting."
+    },
+    {
+        "title": "Monitoring & Reporting",
+        "description": "Effortlessly track and report carbon emissions with automated monitoring, ensuring accuracy and compliance in sustainability reporting."
+    },
+    {
+        "title": "Simplified Certification Process",
+        "description": "Streamline your certification process with our simplified, automated solution, reducing complexity and ensuring faster compliance."
+    },
+    {
+        "title": "AI-Driven Insights",
+        "description": "Leverage AI-driven insights to uncover hidden patterns, optimize sustainability strategies, and drive impactful decision-making."
+    },
+    {
+        "title": "AI-Driven Insights",
+        "description": "Leverage AI-driven insights to uncover hidden patterns, optimize sustainability strategies, and drive impactful decision-making."
+    }
+]
+
+function Blocks() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="grid grid-cols-3 gap-4 w-[1320px]  h-[659px] p-4">
+                {data.map((item, index) => (
+                    <div
+                        key={index}
+                        className="border p-4 border-none rounded-[12px] shadow-md  bg-white"
+                    >
+                        <h3 className="bingo4 text-green-600 font-bold text-2xl">{item.title}</h3>
+                        <p className="bingo1 text-gray-600 font-sans text-base">{item.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+export default Blocks;
